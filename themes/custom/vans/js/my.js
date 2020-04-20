67688d5457d01bda117c1ee3e927649e996d1162
Zahoor Ahmad <zahoor.drupak@gmail.com>
2020-04-20 15:00:18 +0500

    style the notification view

---
+++ themes/custom/vans/js/my.js
@@ -0,0 +1,21 @@
+(function($) {
+    Drupal.behaviors.custome_barrio = {
+    attach : function (context, settings){
+    
+    
+      var video = $('.new-notification');
+      $('.counter').append(video);
+    
+      var video1 = $('#block-vans-views-block-notifications-view-block-1-2');
+        
+      $('div#riaz').append(video1);
+    //	alert("hello");
+    
+    
+    
+    }
+    }
+    
+    
+    })(jQuery);
+    
